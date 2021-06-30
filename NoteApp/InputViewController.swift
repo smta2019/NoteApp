95a95761c215ea17c8b37f62b605c86f402d3c1b
smta2019 <smta201911@gmail.com>
2021-07-01 01:07:48 +0900

    Realmのインストール、gitignoreの作成までコミット。

---
+++ NoteApp/InputViewController.swift
@@ -0,0 +1,36 @@
+//
+//  InputViewController.swift
+//  NoteApp
+//
+//  Created by stma2019 on 2021/07/01.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func verticalButton(_ sender: Any) {
+    }
+    
+    @IBAction func horizontalButton(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.2'
+
+target 'NoteApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NoteApp
+  pod 'RealmSwift','10.1.4'
+
+end
